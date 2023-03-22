b11bbdad82ff32fca78adadbc19e1a518b24479d
Andrey <andrey.liubashchenko@gmail.com>
2023-03-22 11:50:32 +0200

    slider

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -6,6 +6,9 @@ import './modules/handlers.js';
 import './modules/attributes.js';
 import './modules/effects.js';
 import './components/modal.js';
+import './components/tab.js';
+import './components/accordion.js';
+import './components/carousel.js';
 
 
 export default $;
--- src/js/script.js
+++ src/js/script.js
@@ -16,17 +16,50 @@ window.addEventListener('DOMContentLoaded', () => {
 	// 	$('.w-500').fadeToggle(800);
 	// });
 
-	$('.wrap').html(`
-		<div class="dropdown">
-		<button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton1">Dropdown buttons</button>
-		<div class="dropdown-menu" data-toggle-id="dropdownMenuButton1">
-			<a href="#" class="dropdown-item">Action</a>
-			<a href="#" class="dropdown-item">Action #2</a>
-			<a href="#" class="dropdown-item">Action #3</a>
-		</div>
-	</div>
-	`);
-	$('.dropdown-toggle').dropdown();
+	// $('.wrap').html(`
+	// 	<div class="dropdown">
+	// 	<button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton1">Dropdown buttons</button>
+	// 	<div class="dropdown-menu" data-toggle-id="dropdownMenuButton1">
+	// 		<a href="#" class="dropdown-item">Action</a>
+	// 		<a href="#" class="dropdown-item">Action #2</a>
+	// 		<a href="#" class="dropdown-item">Action #3</a>
+	// 	</div>
+	// </div>
+	// `);
+	// $('.dropdown-toggle').dropdown();
+
+	$('#trigger').click(() => $('#trigger').createModal({
+		text: {
+			title: 'Modal title',
+			body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, maxime. Perspiciatis vel maiores facilis ipsa tempore incidunt dolorem dicta velit maxime eum sequi laboriosam omnis veniam, facere pariatur! Labore, vel.'
+		},
+		btns: {
+			count: 3,
+			settings: [
+				[
+					'Close',
+					['btn-danger', 'mr-10'],
+					true
+				],
+				[
+					'Save changes',
+					['btn-success'],
+					false,
+					() => {
+						alert('Done ');
+					}
+				],
+				[
+					'Anither button',
+					['btn-warning', 'ml-10'],
+					false,
+					() => {
+						alert('Hellow');
+					}
+				]
+			]
+		}
+	}));
 });
 
 
